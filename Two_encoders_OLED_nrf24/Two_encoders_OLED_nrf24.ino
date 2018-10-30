4e5a3e0576eb9b86f4aa046384fb2b183cf7fe4a
NosovDE <NosovDE@gmail.com>
2018-10-30 21:52:48 +0300

    Fix

--- Two_encoders_OLED_nrf24/Two_encoders_OLED_nrf24.ino
+++ Two_encoders_OLED_nrf24/Two_encoders_OLED_nrf24.ino
@@ -1,10 +1,7 @@
-
-#define CLK 7
-#define DT 8
-#define SW 9
 #include "GyverEncoder.h"
-Encoder enc1(CLK, DT, SW);
-Encoder enc2(5, 6, 4);
+
+Encoder enc1(5, 6, 4);
+Encoder enc2(7, 8, 9);
 
 
 // OLED экран
@@ -14,7 +11,10 @@ OLED  myOLED(SDA, SCL, 8);
 extern uint8_t RusFont[];
 extern uint8_t SmallFont[];
 extern uint8_t MediumNumbers[];
+extern uint8_t BigNumbers[];
 
+long positionLeft  = 0;
+long positionRight = 0;
 
 
 
@@ -25,53 +25,59 @@ void setup() {
   // enc2.setTickMode(AUTO);
 
   myOLED.begin();
-  myOLED.setFont(SmallFont);
-  myOLED.print("Left", LEFT, 1);
-  myOLED.print("Right", RIGHT, 1);
-
-  myOLED.setFont(MediumNumbers);
-  myOLED.print("0", LEFT, 15);
-  myOLED.print("0", RIGHT, 15);
-  myOLED.update();
+  updateScreen(positionLeft, positionRight);
 
   //  Serial.begin(9600);
   //  Serial.println("TwoKnobs Encoder Test:");
 }
 
-long positionLeft  = 0;
-long positionRight = 0;
 
 void loop() {
   // обязательная функция отработки. Должна постоянно опрашиваться
   enc1.tick();
   enc2.tick();
 
-  //  if (enc1.isTurn()) {     // если был совершён поворот (индикатор поворота в любую сторону)
-  // ваш код
-  //  }
 
   if (enc1.isRight())
   {
     //Serial.println("Right");         // если был поворот
-    positionRight++;
-    updateScreen();
+    positionLeft--;
+    // updateScreen();
   }
   if (enc1.isLeft())
   {
     // Serial.println("Left");
-    positionRight--;
-    updateScreen();
+    positionLeft++;
+    //    updateScreen();
   }
+
+  if (enc1.isRelease()) {
+    positionLeft = 0;
+    updateScreen(positionLeft, positionRight);
+  }
+
+  if (enc1.isTurn()) {     // если был совершён поворот (индикатор поворота в любую сторону)
+    updateScreen(positionLeft, positionRight);
+  }
+
 
   if (enc2.isRight())
   {
-    positionLeft++;
-    updateScreen();
+    positionRight--;
+    //  updateScreen();
   }
   if (enc2.isLeft())
   {
-    positionLeft--;
-    updateScreen();
+    positionRight++;
+    //   updateScreen();
+  }
+  if (enc2.isPress()) {
+    positionRight = 0;
+    updateScreen(positionLeft, positionRight);
+  }
+
+  if (enc2.isTurn()) {     // если был совершён поворот (индикатор поворота в любую сторону)
+    updateScreen(positionLeft, positionRight);
   }
 
 
@@ -88,16 +94,33 @@ void loop() {
 
 }
 
-void updateScreen() {
+
+
+void updateScreen(long left, long right) {
   myOLED.clrScr();
-  
+
+  // Левый квадрат
   myOLED.setFont(SmallFont);
   myOLED.print("Left", LEFT, 1);
-  myOLED.print("Right", RIGHT, 1);
+  myOLED.setFont(RusFont);
+  myOLED.print("Ntvgthfnehf", LEFT + 3, 53);
+  myOLED.setFont(BigNumbers);
+  myOLED.print(String(left), LEFT + 13, 20);
 
-  myOLED.setFont(MediumNumbers);
-  myOLED.print(String(positionLeft), LEFT, 15);
-  myOLED.print(String(positionRight), RIGHT, 15);
+  myOLED.drawRoundRect(0, 16, 65, 63);
+  myOLED.drawLine(0, 50, 65, 50);
+
+  // Правый квадрат
+  myOLED.setFont(SmallFont);
+  myOLED.print("Right", RIGHT, 1);
+  myOLED.setFont(RusFont);
+  myOLED.print("Dkf;yjcnm",  70 , 53);
+  myOLED.setFont(BigNumbers);
+  myOLED.print(String(right), 80, 20);
+
+  myOLED.drawRoundRect(67, 16, 127, 63);
+  myOLED.drawLine(67, 50, 123, 50);
+
 
   myOLED.update();
 }
