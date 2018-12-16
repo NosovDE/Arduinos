d54a40a50cc2c0f5c44a446a89d1db4d6c1912ee
NosovDE <NosovDE@gmail.com>
2018-12-16 23:29:09 +0300

    Fix

---
+++ police/police.ino
@@ -0,0 +1,75 @@
+#include "FastLED.h"          // библиотека для работы с лентой
+
+#define LED_COUNT 4          // число светодиодов в кольце/ленте
+#define LED_DT 6             // пин, куда подключен DIN ленты
+
+int max_bright = 255;         // максимальная яркость (0 - 255)
+
+struct CRGB leds[LED_COUNT];
+int ledsX[LED_COUNT][3];     //-ARRAY FOR COPYING WHATS IN THE LED STRIP CURRENTLY (FOR CELL-AUTOMATA, MARCH, ETC)
+
+int BOTTOM_INDEX = 0;        // светодиод начала отсчёта
+int TOP_INDEX = int(LED_COUNT / 2);
+int EVENODD = LED_COUNT % 2;
+
+int thisdelay = 20;          //-FX LOOPS DELAY VAR
+int thisstep = 10;           //-FX LOOPS DELAY VAR
+int thishue = 0;             //-FX LOOPS DELAY VAR
+int thissat = 255;           //-FX LOOPS DELAY VAR
+
+int thisindex = 0;
+int thisRED = 0;
+int thisGRN = 0;
+int thisBLU = 0;
+
+int idex = 0;                //-LED INDEX (0 to LED_COUNT-1
+int ihue = 0;                //-HUE (0-255)
+int ibright = 0;             //-BRIGHTNESS (0-255)
+int isat = 0;                //-SATURATION (0-255)
+int bouncedirection = 0;     //-SWITCH FOR COLOR BOUNCE (0-1)
+float tcount = 0.0;          //-INC VAR FOR SIN LOOPS
+int lcount = 0;              //-ANOTHER COUNTING VAR
+
+
+void setup() {
+  Serial.begin(9600);              // открыть порт для связи
+  LEDS.setBrightness(max_bright);  // ограничить максимальную яркость
+
+  LEDS.addLeds<WS2811, LED_DT, GRB>(leds, LED_COUNT);  // настрйоки для нашей ленты (ленты на WS2811, WS2812, WS2812B)
+  one_color_all(0, 0, 0);          // погасить все светодиоды
+  LEDS.show();                     // отослать команду
+}
+
+void loop() {
+  ems_lightsSTROBE();
+  delay(100);
+
+}
+
+void ems_lightsSTROBE() {                  //-m26-EMERGENCY LIGHTS (STROBE LEFT/RIGHT)
+  int thishue = 0;
+  int thathue = (thishue + 160) % 255;
+  for (int x = 0 ; x < 5; x++ ) {
+    for (int i = 0 ; i < TOP_INDEX; i++ ) {
+      leds[i] = CHSV(thishue, thissat, 255);
+    }
+    LEDS.show(); delay(thisdelay);
+    one_color_all(0, 0, 0);
+    LEDS.show(); delay(thisdelay);
+  }
+  for (int x = 0 ; x < 5; x++ ) {
+    for (int i = TOP_INDEX ; i < LED_COUNT; i++ ) {
+      leds[i] = CHSV(thathue, thissat, 255);
+    }
+    LEDS.show(); delay(thisdelay);
+    one_color_all(0, 0, 0);
+    LEDS.show(); delay(thisdelay);
+  }
+}
+
+
+void one_color_all(int cred, int cgrn, int cblu) {       //-SET ALL LEDS TO ONE COLOR
+  for (int i = 0 ; i < LED_COUNT; i++ ) {
+    leds[i].setRGB( cred, cgrn, cblu);
+  }
+}
